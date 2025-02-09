7480523caaf0cafc233625d7b1b0a075b15a249b
sejal-123 <sejalbudhani@gmail.com>
2025-02-09 21:44:05 +0530

    Added first typescript file and compiled to js using tsc into.ts command

---
+++ 01Intro/intro.js
@@ -0,0 +1,3 @@
+var user = { name: 'Sejal', age: 24 };
+console.log('Sejal');
+console.log(user.name);
---
+++ 01Intro/intro.ts
@@ -0,0 +1,3 @@
+let user = { name: 'Sejal', age: 24 }
+console.log('Sejal');
+console.log(user.name);
