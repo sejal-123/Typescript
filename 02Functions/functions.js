778945aaf51b32d8dd590d4d012af14ee0701cd4
sejal-123 <sejalbudhani@gmail.com>
2025-02-12 00:25:02 +0530

    Arrow function with default value

--- 02Functions/functions.js
+++ 02Functions/functions.js
@@ -4,3 +4,16 @@ function addNum(num) {
 }
 // addNum('S'); // here as when we don't specify the type, it allows string also to be passed
 addNum(10);
+function toUpper(val) {
+    return val.toUpperCase();
+}
+// toUpper(10);
+toUpper('Sejal');
+function signupUser(username, email, isPaid) {
+    console.log(username, email, isPaid);
+}
+var test = function (username, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+signupUser('sej', 'sej@s.com', true);
+test('s', 's');
--- 02Functions/functions.ts
+++ 02Functions/functions.ts
@@ -5,3 +5,19 @@ function addNum(num: number) {
 
 // addNum('S'); // here as when we don't specify the type, it allows string also to be passed
 addNum(10);
+
+function toUpper(val: string) {
+    return val.toUpperCase();
+}
+
+// toUpper(10);
+toUpper('Sejal');
+
+function signupUser(username: string, email: string, isPaid: boolean){
+    console.log(username, email, isPaid);
+}
+
+let test = (username: string, email: string, isPaid: boolean = false) => {}
+
+signupUser('sej', 'sej@s.com', true);
+test('s', 's');
