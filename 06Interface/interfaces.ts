a311a2e1a59c752d1fe89bc592e65b819ef16ee5
sejal-123 <sejalbudhani@gmail.com>
2025-02-14 00:42:07 +0530

    Difference between interface and type alias

--- 06Interface/interfaces.ts
+++ 06Interface/interfaces.ts
@@ -18,3 +18,32 @@ const usr: User = {
         return ''
     }
 }
+
+// One major difference between type aliases vs interfaces
+// are that interfaces are open and type aliases are closed.
+// This means you can extend an interface by declaring it
+// a second time.
+
+interface Kitten {
+    purrs: boolean;
+}
+
+interface Kitten {
+    colour: string;
+}
+
+// In the other case a type cannot be changed outside of
+// its declaration.
+
+// type Puppy = {
+//     color: string;
+// };
+
+// type Puppy = {
+//     toys: number;
+// };
+  
+
+// Type aliases support extending other type aliases by intersection type. The & symbol is used to create an intersection type. Type aliases also extends interface types.
+
+// Interface Type also supports extending other interfaces by using the `extends` keyword. Interface type also extends type aliases.
